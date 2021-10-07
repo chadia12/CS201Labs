d1359e4dbbe27fd2182b3474b454396ab6126679
chadia12 <80862580+chadia12@users.noreply.github.com>
2021-10-07 15:15:57 -0500

    10/06/2021

--- src/Lab8and9/Question3.java
+++ src/Lab8and9/Question3.java
@@ -1,23 +1,27 @@
 package Lab8and9;
-import java.lang.*;
+
+
 import java.util.Scanner;
 
 public class Question3 {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        String sentence;
-
-        do{
-            System.out.println("sentence");
-            sentence = input.nextLine();
-            sentence = "GATTCGATC";
-            if(sentence.isEmpty())
-                break;
-        String s1= sentence.replace('A','T');
-        String s2= sentence.replace('G','C');
-            System.out.println(" ");
-            System.out.println(s1 +"[ ]"+ s2);
-
+        Scanner key = new Scanner(System.in);
+        do {
+            System.out.println("Enter a sentence ");
+            String sentence = key.nextLine();
+            if(sentence.isEmpty()){
+                break;}
+            StringBuffer sent = new StringBuffer(sentence);
+            sent.setCharAt(0, 'C');
+            sent.setCharAt(1, 'T');
+            sent.setCharAt(2, 'A');
+            sent.setCharAt(3, 'A');
+            sent.setCharAt(4, 'G');
+            sent.setCharAt(5, 'C');
+            sent.setCharAt(6, 'T');
+            sent.setCharAt(7, 'A');
+            sent.setCharAt(8, 'G');
+            System.out.println(sent);
         }while(true);
     }
 }
